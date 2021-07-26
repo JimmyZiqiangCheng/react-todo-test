127cf0d0b3901cb9b3cae5f2a9c811322d72e416
Jimmy Cheng <jimmycheng@JimmydeMac-mini.local>
2021-07-26 13:10:47 +0800

    todo form populated

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,6 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import { setSyntheticLeadingComments } from "typescript";
+
 const Todo = () =>{
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('javascript');
+    const [content, setContent] = useState('');
+
+    const handleSubmit = e => {
+
+    }
+
     return(
-        <h1>Todo works</h1>
+        <form onSubmit={handleSubmit}>
+        <label>
+            Description:
+            <input type="text" 
+            name="description" 
+            value={description} 
+            onChange = {e=>setDescription(e.target.value)}
+            required
+            />
+        </label>
+        <label>
+            Category:
+            <select
+                value={category}
+                onChange = {e=>setCategory(e.target.value)}
+            >
+                <option value="javascript">javascript</option>
+                <option value="css">css</option>
+            </select>
+        </label>
+        <label>
+            Content:
+            <textarea 
+                name="content" 
+                onChange = {e=>setContent(e.target.value)}
+                value = {content}
+                required
+            />
+        </label>
+        <button>Submit</button>
+        </form>
     )
 }
 export default Todo;
